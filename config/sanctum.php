06418df8c18aacbfba7503b7861e75ac82d62217
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-07-03 13:01:37 +1200

    Whitelist triptime.cc for authentication

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,triptime.cc')),
 
     /*
     |--------------------------------------------------------------------------
